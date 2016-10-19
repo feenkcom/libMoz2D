28791305692ea5cd72437e13af6cac9af1b48894
Aliaksei Syrel <alex@syrel.ch>
2016-10-19 21:33:41 +0200

    Source surface mapping

--- src/source_surface.cpp
+++ src/source_surface.cpp
@@ -12,11 +12,34 @@ DataSourceSurface* moz2d_data_source_surface_create(SurfaceFormat aFormat, int32
 	return Factory::CreateDataSourceSurface(IntSize(width, height), aFormat,  isZero).take();
 }
 
-DataSourceSurface::MappedSurface* moz2d_source_surface_map(DataSourceSurface* surface, DataSourceSurface::MapType aMapType) {
+uint8_t * moz2d_source_surface_map(DataSourceSurface* surface, int32_t * retStride) {
 	DataSourceSurface::MappedSurface mapping;
-	if (!surface->Map(DataSourceSurface::MapType::WRITE, &mapping)) {
+	if (!surface->Map(DataSourceSurface::MapType::READ, &mapping)) {
 		gfxCriticalError() << "moz2d_source_surface_map failed to map surface";
+		if (retStride) *retStride = 0;
 		return nullptr;
 	}
-	return &mapping;
+	if (retStride) *retStride = mapping.mStride;
+	return mapping.mData;
+}
+
+void moz2d_source_surface_unmap(DataSourceSurface* surface) {
+	surface->Unmap();
+}
+
+void moz2d_source_surface_copy(uint8_t * aSource, int32_t aSourceStride, uint8_t * aDest, int32_t aDestStride, int32_t x, int32_t y, int32_t width, int32_t height) {
+	uint32_t *sourceData = (uint32_t*) aSource;
+	uint32_t *destData = (uint32_t*) aDest;
+	int32_t sourceWidth = aSourceStride / 4;
+	int32_t destWidth = aDestStride / 4;
+	int32_t sourceOffset = y * sourceWidth + x;
+	int32_t destOffset = y * destWidth + x;
+
+	uint32_t bufferSize = width * 4;
+
+	for (int32_t i = 0; i < height; ++i) {
+		memcpy(destData + destOffset, sourceData + sourceOffset, bufferSize);
+		sourceOffset += sourceWidth;
+		destOffset += destWidth;
+	}
 }
--- src/source_surface.h
+++ src/source_surface.h
@@ -18,7 +18,9 @@ extern "C" {
 #endif
 
 LIBRARY_API DataSourceSurface* moz2d_data_source_surface_create(SurfaceFormat aFormat, int32_t width, int32_t height, bool isZero);
-LIBRARY_API DataSourceSurface::MappedSurface* moz2d_source_surface_map(DataSourceSurface* surface, DataSourceSurface::MapType aMapType);
+LIBRARY_API uint8_t * moz2d_source_surface_map(DataSourceSurface* surface, int32_t * retStride);
+LIBRARY_API void moz2d_source_surface_copy(uint8_t * aSource, int32_t aSourceStride, uint8_t * aDest, int32_t aDestStride, int32_t x, int32_t y, int32_t width, int32_t height);
+LIBRARY_API void moz2d_source_surface_unmap(DataSourceSurface* surface);
 
 #ifdef __cplusplus
 }
