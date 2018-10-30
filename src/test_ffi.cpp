7cfff8acb57a6dca04992f76e50b18a843c4b36f
Aliaksei Syrel <alex.syrel@gmail.com>
2018-10-30 22:27:38 +0100

    Add floating point numbers test

--- src/test_ffi.cpp
+++ src/test_ffi.cpp
@@ -7,6 +7,9 @@
 #define FFI_SOURCE_FOR_EACH(prefix, type) LIBRARY_API type FFI_FUNCTION_NAME(prefix, type) (type aValue) { return aValue; };
 #define FFI_SOURCE(prefix, ...) FOR_EACH(FFI_SOURCE_FOR_EACH, prefix, __VA_ARGS__)
 
+#define FFI_SOURCE_COMBO_FOR_EACH(prefix, type) LIBRARY_API bool FFI_FUNCTION_NAME(prefix, type) (type aValue1, type aValue2, type aValue3, type aValue4, type aValue5) { return aValue1 == aValue2 && aValue2 == aValue3 && aValue3 == aValue4 && aValue4 == aValue5; };
+#define FFI_SOURCE_COMBO(prefix, ...) FOR_EACH(FFI_SOURCE_COMBO_FOR_EACH, prefix, __VA_ARGS__)
+
 #define FFI_SOURCE_FOR_EACH_ASSOCIATION(prefix, type, value) type FFI_FUNCTION_NAME(prefix, type) () { return value; };
 #define FFI_SOURCE_ASSOCIATION(prefix, ...) PAIRS_DO(FFI_SOURCE_FOR_EACH_ASSOCIATION, prefix, __VA_ARGS__)
 
@@ -20,10 +23,12 @@ FFI_SOURCE_ASSOCIATION(moz2d_test_ffi_fixed_unsigned_max_, FFI_TYPES_FIXED_UNSIG
 
 FFI_CALLBACK_SOURCE(moz2d_test_ffi_callback_, FFI_TYPES_FIXED)
 
-
 FFI_SOURCE(moz2d_test_ffi_, FFI_TYPES)
+FFI_SOURCE_COMBO(moz2d_test_ffi_combo_, FFI_TYPES)
 FFI_SOURCE_ASSOCIATION(moz2d_test_ffi_signed_min_, FFI_TYPES_SIGNED_MIN)
 FFI_SOURCE_ASSOCIATION(moz2d_test_ffi_signed_max_, FFI_TYPES_SIGNED_MAX)
 FFI_SOURCE_ASSOCIATION(moz2d_test_ffi_unsigned_max_, FFI_TYPES_UNSIGNED_MAX)
+FFI_SOURCE_ASSOCIATION(moz2d_test_ffi_floating_min_, FFI_TYPES_FLOATING_MIN)
+FFI_SOURCE_ASSOCIATION(moz2d_test_ffi_floating_max_, FFI_TYPES_FLOATING_MAX)
 
 FFI_CALLBACK_SOURCE(moz2d_test_ffi_callback_, FFI_TYPES)
--- src/test_ffi.h
+++ src/test_ffi.h
@@ -9,11 +9,15 @@
 #include "macros.h"
 #include <stdint.h>
 #include <limits.h>
+#include <float.h>
 
 #define FFI_FUNCTION_NAME(prefix, body) CONCATENATE(prefix, body)
 
 #define FFI_HEADER_FOR_EACH(prefix, type) LIBRARY_API type FFI_FUNCTION_NAME(prefix, type) (type aValue);
 #define FFI_HEADER(prefix, ...) FOR_EACH(FFI_HEADER_FOR_EACH, prefix, __VA_ARGS__)
+
+#define FFI_HEADER_COMBO_FOR_EACH(prefix, type) LIBRARY_API bool FFI_FUNCTION_NAME(prefix, type) (type aValue1, type aValue2, type aValue3, type aValue4, type aValue5);
+#define FFI_HEADER_COMBO(prefix, ...) FOR_EACH(FFI_HEADER_COMBO_FOR_EACH, prefix, __VA_ARGS__)
 
 #define FFI_HEADER_FOR_EACH_ASSOCIATION(prefix, type, value) LIBRARY_API type FFI_FUNCTION_NAME(prefix, type) ();
 #define FFI_HEADER_ASSOCIATION(prefix, ...) PAIRS_DO(FFI_HEADER_FOR_EACH_ASSOCIATION, prefix, __VA_ARGS__)
@@ -30,11 +34,14 @@ typedef unsigned char uchar;
 typedef unsigned short ushort;
 typedef unsigned int uint;
 typedef unsigned long ulong;
+typedef long double ldouble;
 
-#define FFI_TYPES char, uchar, short, ushort, int, uint, long, ulong
+#define FFI_TYPES char, uchar, short, ushort, int, uint, long, ulong, float, double, ldouble
 #define FFI_TYPES_SIGNED_MIN char, CHAR_MIN, short, SHRT_MIN, int, INT_MIN, long, LONG_MIN
 #define FFI_TYPES_SIGNED_MAX char, CHAR_MAX, short, SHRT_MAX, int, INT_MAX, long, LONG_MAX
 #define FFI_TYPES_UNSIGNED_MAX uchar, UCHAR_MAX, ushort, USHRT_MAX, uint, UINT_MAX, ulong, ULONG_MAX
+#define FFI_TYPES_FLOATING_MIN float, FLT_MIN, double, DBL_MIN, ldouble, LDBL_MIN
+#define FFI_TYPES_FLOATING_MAX float, FLT_MAX, double, DBL_MAX, ldouble, LDBL_MAX
 
 #ifdef __cplusplus
 extern "C" {
@@ -46,9 +53,12 @@ FFI_HEADER_ASSOCIATION(moz2d_test_ffi_fixed_signed_max_, FFI_TYPES_FIXED_SIGNED_
 FFI_HEADER_ASSOCIATION(moz2d_test_ffi_fixed_unsigned_max_, FFI_TYPES_FIXED_UNSIGNED_MAX)
 
 FFI_HEADER(moz2d_test_ffi_, FFI_TYPES)
+FFI_HEADER_COMBO(moz2d_test_ffi_combo_, FFI_TYPES)
 FFI_HEADER_ASSOCIATION(moz2d_test_ffi_signed_min_, FFI_TYPES_SIGNED_MIN)
 FFI_HEADER_ASSOCIATION(moz2d_test_ffi_signed_max_, FFI_TYPES_SIGNED_MAX)
 FFI_HEADER_ASSOCIATION(moz2d_test_ffi_unsigned_max_, FFI_TYPES_UNSIGNED_MAX)
+FFI_HEADER_ASSOCIATION(moz2d_test_ffi_floating_min_, FFI_TYPES_FLOATING_MIN)
+FFI_HEADER_ASSOCIATION(moz2d_test_ffi_floating_max_, FFI_TYPES_FLOATING_MAX)
 
 FFI_CALLBACK_HEADER(moz2d_test_ffi_callback_, FFI_TYPES_FIXED)
 FFI_CALLBACK_HEADER(moz2d_test_ffi_callback_, FFI_TYPES)
