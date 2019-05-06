5ffa11a2eb4f9e7236cfb578d5f63dee81451dab
Aliaksei Syrel <alex.syrel@gmail.com>
2019-05-06 03:27:42 +0200

    Test

--- example_powershell_script.ps1
+++ example_powershell_script.ps1
@@ -1,8 +1,3 @@
 # A super-basic example PowerShell script
 
-Write-Host 'Hello, world!'
-
 Get-Date -Format "dd-MM-yyyy HH:mm"
-
-# List CPU cores on this box
-WmiObject -class win32_processor -Property Name, NumberOfCores, NumberOfLogicalProcessors | Format-List -Property Name, NumberOfCores, NumberOfLogicalProcessors 
