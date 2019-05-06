8484e69cbdbb64988223ff3fc0afc5cab187dd68
Aliaksei Syrel <alex.syrel@gmail.com>
2019-05-06 04:12:32 +0200

    Test

--- windows-toolkit.sh
+++ windows-toolkit.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 mkdir -p mozilla-build
-curl https://ftp.mozilla.org/pub/mozilla/libraries/win32/MozillaBuildSetup-2.2.0.exe --output mozilla-build/MozillaBuildSetup-2.2.0.exe
-7z x mozilla-build/MozillaBuildSetup-2.2.0.exe -omozilla-build
+ls -la
 
-./mozilla-build/start-shell-msvc2015-x64.bat
+#curl https://ftp.mozilla.org/pub/mozilla/libraries/win32/MozillaBuildSetup-2.2.0.exe --output mozilla-build/MozillaBuildSetup-2.2.0.exe
+#7z x mozilla-build/MozillaBuildSetup-2.2.0.exe -omozilla-build
+
+#./mozilla-build/start-shell-msvc2015-x64.bat
