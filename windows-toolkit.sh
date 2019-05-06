6961f0c60dea8bcc062c411db8fbfd248e0743bb
Aliaksei Syrel <alex.syrel@gmail.com>
2019-05-06 04:23:12 +0200

    test

--- windows-toolkit.sh
+++ windows-toolkit.sh
@@ -6,6 +6,20 @@ ls -la
 curl https://ftp.mozilla.org/pub/mozilla/libraries/win32/MozillaBuildSetup-2.2.0.exe --output mozilla-build/MozillaBuildSetup-2.2.0.exe
 #wget -O mozilla-build/MozillaBuildSetup-2.2.0.exe https://ftp.mozilla.org/pub/mozilla/libraries/win32/MozillaBuildSetup-2.2.0.exe
 
-#7z x mozilla-build/MozillaBuildSetup-2.2.0.exe -omozilla-build
+7z x mozilla-build/MozillaBuildSetup-2.2.0.exe -omozilla-build
 
-#./mozilla-build/start-shell-msvc2015-x64.bat
+echo "if [ -f ~/.bashrc ]; then . ~/.bashrc; fi" >> .bash_profile
+
+cat >> .bashrc <<EOL
+if [[ \`"$STARTUP_DIR\`" != \`"$PWD\`" ]]; then
+    cd $STARTUP_DIR
+fi
+export PATH=`$PATH:/c/Program\ Files/nodejs
+export PATH=`$PATH:/c/Program\ Files/CMake/bin
+export PATH=~/$MOZILLA_BUILD/bin:`$PATH
+./build.sh
+EOL
+
+ls -la
+
+./mozilla-build/start-shell-msvc2015-x64.bat
